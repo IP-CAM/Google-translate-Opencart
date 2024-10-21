4e193fe3972e7a5e771a9739d243e7e4aa636bb7
Nick <nick.hlushpenko@gmail.com>
2024-10-21 07:50:40 +0300

    add settings and tiny library

---
+++ upload/admin/language/uk-ua/extension/module/google_translate.php
@@ -0,0 +1,11 @@
+<?php
+
+$_['heading_name']  = 'Google Translate';
+$_['heading_title'] = '<a style="color:#fff; background: #1DB954; padding: 8px 16px;border-radius: 4px;" href="https://hlushpenko.top" target="_blank">Nick Hlushpenko</a> ' . $_['heading_name'];
+
+$_['entry_status']     = 'Статус';
+$_['text_extension']    = 'Розширення';
+$_['entry_google_api_key']     = 'API Key';
+$_['text_edit']         = 'Змінити налаштування';
+$_['text_success']      = 'Налаштування успішно змінено!';
+$_['error_permission'] = 'Немає прав для управління модулем!';
---
+++ upload/system/library/google_translate.php
@@ -0,0 +1,38 @@
+<?php
+
+class LibreTranslate {
+
+    function translate($text, $sourceLang, $targetLang, $apiKey) {
+
+        $url = 'https://translation.googleapis.com/language/translate/v2?key=' . $apiKey;
+
+        $data = array(
+            'q' => $text,
+            'source' => $sourceLang,
+            'target' => $targetLang,
+            'format' => 'text',
+        );
+
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/json\r\n",
+                'method'  => 'POST',
+                'content' => json_encode($data),
+            ),
+        );
+
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+
+        if ($result === FALSE) {
+            return 'Error: Не вдалося підключитися до Google Translate API';
+        }
+
+        $response = json_decode($result, true);
+
+        return isset($response['data']['translations'][0]['translatedText'])
+            ? $response['data']['translations'][0]['translatedText']
+            : 'Error: Не вдалося виконати переклад';
+    }
+
+}
