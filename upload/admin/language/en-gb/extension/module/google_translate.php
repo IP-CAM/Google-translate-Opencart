82ac4d2e398ab8e07245d236229ab0ab72088a0c
Nick <nick.hlushpenko@gmail.com>
2024-10-21 09:18:33 +0300

    add english translations of admin && add description translate button

---
+++ upload/admin/language/en-gb/extension/module/google_translate.php
@@ -0,0 +1,11 @@
+<?php
+
+$_['heading_name']  = 'Google Translate';
+$_['heading_title'] = '<a style="color:#fff; background: #1DB954; padding: 8px 16px;border-radius: 4px;" href="https://hlushpenko.top" target="_blank">Nick Hlushpenko</a> ' . $_['heading_name'];
+
+$_['entry_status']             = 'Status';
+$_['text_extension']           = 'Extensions';
+$_['entry_google_api_key']     = 'API Key';
+$_['text_edit']                = 'Change settings';
+$_['text_success']             = 'Settings saved!';
+$_['error_permission']         = 'You do not have permission to change Google Translate!';
